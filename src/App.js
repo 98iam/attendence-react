aad20ab4a66b3dd94ce1486e672c4e3044a65520
toba <gago23@gmail.com>
2025-07-20 06:44:56 +0530

    Initial React project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+      <p>Your React project is ready to go.</p>
+    </div>
+  );
+}
+
+export default App;
